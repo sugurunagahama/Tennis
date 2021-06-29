b0b5409e9b52c4330789f274f3a5e10e1de51335
suguru <ec2-user@ip-172-31-18-186.us-east-2.compute.internal>
2021-06-29 08:41:17 +0000

    00

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Tennis
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.load_defaults 5.0
+    #config.load_defaults 5.0
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
 end
