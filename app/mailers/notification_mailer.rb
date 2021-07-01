a841d8eed96527eb22304be910daefa25d9121f8
suguru <ec2-user@ip-172-31-18-186.us-east-2.compute.internal>
2021-07-01 19:32:48 +0000

    oooo

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,7 +3,7 @@ class NotificationMailer < ApplicationMailer
 
   def complete_mail(reservation)
     @reservation = reservation
-    @url = "https://48f9c8cefb7d4d25a1cc378228653507.vfs.cloud9.us-east-2.amazonaws.com/"
+    @url = "https://agile-garden-00137.herokuapp.com/"
     mail(subject: "[tennisReservation]コートの予約が完了しました。" ,to: @reservation.email)
   end
 end
