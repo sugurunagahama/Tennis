6ab0a9aff805a799fcc67fe19ed5d48e1673bed3
suguru <ec2-user@ip-172-31-18-186.us-east-2.compute.internal>
2021-07-01 16:27:18 +0000

    3333

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,17 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+if Rails.env.production?
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'sygyrubucket'
+  config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIASIAUVOSYTLVIJE3K',
-    aws_secret_access_key: 'tSAPmXHWBnM2utwUoqK ZhlY82rqLeW0nzh03sXR3',
-    region: 'ap-northeast-1',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_DEFAULT_REGION'],
     path_style: true
   }
+end
 end
