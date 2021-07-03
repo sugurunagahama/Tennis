f47f3dea45df0388d1faa82d8e5263a4793b740f
suguru <ec2-user@ip-172-31-18-186.us-east-2.compute.internal>
2021-07-03 06:41:22 +0000

    333

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,4 @@
-if Rails.env.production? then
+if Rails.env.production? 
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
@@ -11,11 +11,6 @@ Geocoder.configure(
    api_key: ENV['GOOGLE_MAP_API_KEY']            # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
-else
-Geocoder.configure(
-  lookup: :google,
-  api_key: ENV['GOOGLE_MAP_API_KEY']
-end
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
   # supports SocketError and Timeout::Error
